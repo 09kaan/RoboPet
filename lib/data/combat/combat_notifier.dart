e7d195aa235a374b19ed0d91a97051f3e8368796
Kaan Durukan <kaandurukan09@hotmail.com>
2026-06-12 15:24:35 +0300

    Fix missing providers import in combat_notifier

--- lib/data/combat/combat_notifier.dart
+++ lib/data/combat/combat_notifier.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../app/providers.dart';
 import '../../domain/combat/resolved_stats.dart';
 import '../models/game_item.dart';
 import '../models/robot_instance.dart';
